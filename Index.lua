bdc9a9059bdd020b3b335303e62a6dd69d109dd0
871041532 <871041532@outlook.com>
2019-12-12 00:19:25 +0800

    # 整理接口，提交完整版
    # 整理接口，提交完整版

--- Index.lua
+++ Index.lua
@@ -1,17 +1,115 @@
 local ZMatch = require("ZMatch")
-ZMatch.InitTestEnvironment()
+
+local zmatch
+local t1
+local t2
+local t3
+local count
+local r
+
+function InitTestEnvironment()
+	zmatch = ZMatch.New()
+	t1 = os.clock()
+	zmatch:BuildTrie()
+	t2 = os.clock()
+	print("构建Trie耗时:", t2 - t1)
+	t1 = os.clock()
+	zmatch:BuildAC()
+	t2 = os.clock()
+	print("构建AC耗时:", t2 - t1)
+	local count = 0
+	for _,_ in pairs(zmatch.originCfg) do
+		count = count + 1
+	end
+	print("敏感词总量", count)
+	print("常规词数量", zmatch.singleCount)
+	print("带&词数量", #zmatch.multiList)
+end
+local printLine = function()
+	print("----")
+end
+
+function TestCheck(text, newWayCount, oldWayCOunt)
+	print(string.format("\n\n开始对【%s】进行敏感词检测...", text))
+	printLine()
+	count = oldWayCOunt
+	t2 = os.clock()
+	for i=1,count do
+		r = zmatch:_CheckAllByTraverse(text)
+	end
+	t3 = os.clock()
+	print(string.format("老接口%d次全词检测,时间:%f,结果:%s", count, t3 - t2, r and "true" or "false"))
+	count = newWayCount
+	t2 = os.clock()
+	for i=1,count do
+		r = zmatch:CheckTextByAC(text)
+	end
+	t3 = os.clock()
+	print(string.format("最终接口%d次全词检测,时间:%f,结果:%s", count, t3 - t2, r and "true" or "false"))
+	printLine()
+	local chars = string.ConvertToCharArray(text)
+	t2 = os.clock()
+	for i=1,count do
+		r = zmatch:_CheckSingleByTrie(chars)
+	end
+	t3 = os.clock()
+	print(string.format("\t%d次常规词Trie检测,时间:%f,结果:%s", count, t3 - t2, r and "true" or "false"))
+	t2 = os.clock()
+	for i=1,count do
+		r = zmatch:_CheckSingleByAC(chars)
+	end
+	t3 = os.clock()
+	print(string.format("\t%d次常规词AC检测,时间:%f,结果:%s", count, t3 - t2, r and "true" or "false"))
+	printLine()
+	t2 = os.clock()
+	for i=1,count do
+		r = zmatch:_CheckMultiByTrie(chars)
+	end
+	t3 = os.clock()
+	print(string.format("\t%d次带&词Trie检测,时间%f,结果%s", count, t3 - t2, r and "true" or "false"))
+	t2 = os.clock()
+	for i=1,count do
+		r = zmatch:_CheckMultiByTraverse(text)
+	end
+	t3 = os.clock()
+	print(string.format("\t%d次带&词遍历检测,时间%f,结果%s", count, t3 - t2, r and "true" or "false"))
+end
+
+function TestFilter(text, newWayCount, oldWayCOunt)
+	count = newWayCount
+	t2 = os.clock()
+	for i=1,count do
+		r = zmatch:FilterText(text)
+	end
+	t3 = os.clock()
+	print(string.format("\n\n%d次敏感词过滤,时间:%f,\n--源:【%s】\n--结果:【%s】", count, t3 - t2, text, r))
+	local chars = string.ConvertToCharArray(text)
+	t2 = os.clock()
+	for i=1,count do
+		chars = zmatch:_FilterSingleChars(chars)
+	end
+	t3 = os.clock()
+	print(string.format("\t%d次常规词过滤,时间%f", count, t3 - t2))
+
+	t2 = os.clock()
+	for i=1,count do
+		chars = zmatch:_FilterMultiChars(chars)
+	end
+	t3 = os.clock()
+	print(string.format("\t%d次带&词过滤,时间%f", count, t3 - t2))
+end
+
+
+InitTestEnvironment()
 -- 测试敏感词检测
-local textString = "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
-textString = [[苍天有井独自空， 星落天川遥映瞳。
-小溪流泉映花彩， 松江孤岛一叶枫。
-南海涟波潭边杏， 星空野尽明日辉
-西塞山野雁自翔， 小桥水泽浸芳园。
-武园枯藤空留兰， 李氏眉宇尽是春。]]
+TestCheck("正常说一句话的内容,大概这么长", 1000, 10)
+TestCheck("敏感词:苍井空-", 1000, 10)
+TestCheck("带&敏感词:-咳咳井空苍-", 1000, 10)
+local textString = [[长字符串: 苍天有井独自空, 星落天川遥映瞳。
+小溪流泉映花彩, 松江孤岛一叶枫。
+南海涟波潭边杏, 星空野尽明日辉
+西塞山野雁自翔, 小桥水泽浸芳园。
+武园枯藤空留兰, 李氏眉宇尽是春。]]
 -- local textString = ""
-ZMatch.TestCheck(textString, 1000, 10)
-ZMatch.TestCheck("1咳咳dasdad井空苍1", 1000, 10)
-ZMatch.TestCheck("苍井空", 1000, 10)
-ZMatch.TestCheck("苍井空1", 1000, 10)
-ZMatch.TestCheck("1苍井空", 1000, 10)
-ZMatch.TestCheck("1苍井空1", 1000, 10)
-ZMatch.TestFilter("心如苍井空似水，意比松岛枫叶飞；窗外武藤兰花香，情似饭岛爱相随, 咳咳dasdad井空苍苍", 1000, 1)
+TestCheck(textString, 1000, 10)
+TestFilter("心如苍井空似水,意比松岛枫叶飞。窗外武藤兰花香, 情似饭岛爱相随。咳咳dasdad井空苍苍, 台台ott", 1000, 1)
--- ZMatch.lua
+++ ZMatch.lua
@@ -19,13 +19,14 @@ function ZMatch:ctor()
 	self._multiFilterTrie = MultiFilterTrie.New(self._multiTrie)
 	self.multiList = {}  -- item是数组
 	-- 原始配置表
-	self.originCfg = nil
+	self.originCfg = {}
 
 	self._havenBuildTrie = false
 	self._havenBuildAc = false
 end
 
--- build check的data
+-------------------外部Trie查找接口---------------------------
+-- 必须先BuildTrie再check或filter才有效
 function ZMatch:BuildTrie()
 	if self._havenBuildTrie then
 		return
@@ -45,7 +46,29 @@ function ZMatch:BuildTrie()
 	end
 end
 
--- build AC
+-- 通用接口, 使用Trie检测屏蔽字
+function ZMatch:CheckText(text)
+	local chars = string.ConvertToCharArray(text)
+	return self.singleTrie:CheckCharArrayMatched(chars) or self._multiTrie:CheckCharArrayMatched(chars)
+end
+
+-- 通用接口, 过滤敏感词
+function ZMatch:FilterText(text)
+	local chars = string.ConvertToCharArray(text)
+	local chars, matched1 = self.filterTrie:FilterChars(chars)
+	local chars, matched2 = self._multiFilterTrie:FilterChars(chars)
+	local matched = matched1 or matched2
+	if matched then
+		return table.concat(chars), true
+	end
+	return text, false
+end
+--------------------------------------------------------------------------------
+
+
+--------------------------------外部AC查找接口------------------------------------
+-- 必须在BuildTrie之后再调用BuildAC, 然后再CheckTextByAC才有效
+-- 为什么要独立出来? AC构建的时候会消耗额外时间,短字符串查找没必要使用
 function ZMatch:BuildAC()
 	if self._havenBuildAc then
 		return
@@ -58,13 +81,22 @@ function ZMatch:BuildAC()
 	self.singleTrie:BuildAC()
 end
 
+-- 通用的接口, 使用AC检测屏蔽字
+function ZMatch:CheckTextByAC(text)
+	local chars = string.ConvertToCharArray(text)
+	return self.singleTrie:CheckCharArrayMatchedByAC(chars) or self._multiTrie:CheckCharArrayMatched(chars)
+end
+----------------------------------------------------------------------------------
+
+
+------------------------下面是一些方便测试用的接口,可有可无---------------------------
 -- &多词匹配:使用trie
-function ZMatch:CheckMultiByTrie(chars)
+function ZMatch:_CheckMultiByTrie(chars)
 	return self._multiTrie:CheckCharArrayMatched(chars)
 end
 
 -- &多词匹配:使用遍历 
-function ZMatch:CheckMultiByTraverse(text)
+function ZMatch:_CheckMultiByTraverse(text)
 	local list = self.multiList
 	for _, item in ipairs(list) do
 		local m = true
@@ -82,40 +114,22 @@ function ZMatch:CheckMultiByTraverse(text)
 end
 
 -- 单词匹配:使用Trie
-function ZMatch:CheckSingleByTrie(chars)
+function ZMatch:_CheckSingleByTrie(chars)
 	return self.singleTrie:CheckCharArrayMatched(chars)
 end
 
 -- 单词匹配:使用AC
-function ZMatch:CheckSingleByAC(chars)
+function ZMatch:_CheckSingleByAC(chars)
 	return self.singleTrie:CheckCharArrayMatchedByAC(chars)
 end
 
--- 通用的接口, 内部根据具体情况选择最优匹配算法
-function ZMatch:CheckText(text)
-	local chars = string.ConvertToCharArray(text)
-	return self.singleTrie:CheckCharArrayMatchedByAC(chars) or self._multiTrie:CheckCharArrayMatched(chars)
-end
-
--- 使用Trie替换敏感词(暂时只有单个词的检测替换, 带&的多词后续添加)
-function ZMatch:FilterText(text)
-	local chars = string.ConvertToCharArray(text)
-	local chars, matched1 = self.filterTrie:FilterChars(chars)
-	local chars, matched2 = self._multiFilterTrie:FilterChars(chars)
-	local matched = matched1 or matched2
-	if matched then
-		return table.concat(chars), true
-	end
-	return text, false
-end
-
 -- 带&词过滤
-function ZMatch:FilterMultiChars(chars)
+function ZMatch:_FilterMultiChars(chars)
 	return self._multiFilterTrie:FilterChars(chars)
 end
 
 -- 非&词过滤
-function ZMatch:FilterSingleChars(chars)
+function ZMatch:_FilterSingleChars(chars)
 	return self.filterTrie:FilterChars(chars)
 end
 
@@ -123,7 +137,7 @@ end
 local sensitiveFunc = function(item)
 	return string.split(item.word, "&")
 end
-function ZMatch:CheckAllByTraverse(text)
+function ZMatch:_CheckAllByTraverse(text)
 	local sheet = self.originCfg
 	for _, item in pairs(sheet) do
 		local m = true
@@ -139,104 +153,6 @@ function ZMatch:CheckAllByTraverse(text)
 	end
 	return false
 end
-
-------------------------------下面全是测试代码,正式使用的时候可以删掉----------------------------
-
-local zmatch
-local t1
-local t2
-local t3
-local count
-local r
-function ZMatch.InitTestEnvironment()
-	zmatch = ZMatch.New()
-	t1 = os.clock()
-	zmatch:BuildTrie()
-	t2 = os.clock()
-	print("构建Trie耗时:", t2 - t1)
-	t1 = os.clock()
-	zmatch:BuildAC()
-	t2 = os.clock()
-	print("构建AC耗时:", t2 - t1)
-	local count = 0
-	for _,_ in pairs(zmatch.originCfg) do
-		count = count + 1
-	end
-	print("敏感词总量", count)
-	print("常规词数量", zmatch.singleCount)
-	print("带&词数量", #zmatch.multiList)
-end
-local printLine = function()
-	print("----")
-end
-function ZMatch.TestCheck(text, newWayCount, oldWayCOunt)
-	print(string.format("\n\n开始对【%s】进行敏感词检测...", text))
-	printLine()
-	count = oldWayCOunt
-	t2 = os.clock()
-	for i=1,count do
-		r = zmatch:CheckAllByTraverse(text)
-	end
-	t3 = os.clock()
-	print(string.format("老接口%d次全词检测,时间:%f,结果:%s", count, t3 - t2, r and "true" or "false"))
-	count = newWayCount
-	t2 = os.clock()
-	for i=1,count do
-		r = zmatch:CheckText(text)
-	end
-	t3 = os.clock()
-	print(string.format("最终接口%d次全词检测,时间:%f,结果:%s", count, t3 - t2, r and "true" or "false"))
-	printLine()
-	local chars = string.ConvertToCharArray(text)
-	t2 = os.clock()
-	for i=1,count do
-		r = zmatch:CheckSingleByTrie(chars)
-	end
-	t3 = os.clock()
-	print(string.format("\t%d次常规词Trie检测,时间:%f,结果:%s", count, t3 - t2, r and "true" or "false"))
-	t2 = os.clock()
-	for i=1,count do
-		r = zmatch:CheckSingleByAC(chars)
-	end
-	t3 = os.clock()
-	print(string.format("\t%d次常规词AC检测,时间:%f,结果:%s", count, t3 - t2, r and "true" or "false"))
-	printLine()
-	t2 = os.clock()
-	for i=1,count do
-		r = zmatch:CheckMultiByTrie(chars)
-	end
-	t3 = os.clock()
-	print(string.format("\t%d次带&词Trie检测,时间%f,结果%s", count, t3 - t2, r and "true" or "false"))
-	t2 = os.clock()
-	for i=1,count do
-		r = zmatch:CheckMultiByTraverse(text)
-	end
-	t3 = os.clock()
-	print(string.format("\t%d次带&词遍历检测,时间%f,结果%s", count, t3 - t2, r and "true" or "false"))
-end
-
-function ZMatch.TestFilter(text, newWayCount, oldWayCOunt)
-	count = newWayCount
-	t2 = os.clock()
-	for i=1,count do
-		r = zmatch:FilterText(text)
-	end
-	t3 = os.clock()
-	print(string.format("\n\n%d次敏感词过滤,时间:%f,\n--源:【%s】\n--结果:【%s】", count, t3 - t2, text, r))
-	local chars = string.ConvertToCharArray(text)
-	t2 = os.clock()
-	for i=1,count do
-		chars = zmatch:FilterSingleChars(chars)
-	end
-	t3 = os.clock()
-	print(string.format("\t%d次常规词过滤,时间%f", count, t3 - t2))
-
-	t2 = os.clock()
-	for i=1,count do
-		chars = zmatch:FilterMultiChars(chars)
-	end
-	t3 = os.clock()
-	print(string.format("\t%d次带&词过滤,时间%f", count, t3 - t2))
-end
+-----------------------------------------------------------------
 
 return ZMatch
